ced7c0b121a23eb336afd2b396a2ed76cad9b1c5
Xavier Zachary <xiaxuanzhou@qq.com>
2016-03-31 16:36:48 +0800

    加载相册图片

---
+++ 加载相册图片/Configuration/XXZFontAndColor.h
@@ -0,0 +1,33 @@
+//
+//  XXZFontAndColor.h
+//  Author From Xavier Zachary
+//
+//  Created by Zachary on 14/4/10.
+//  Copyright © 2015年 com.xxzd.future. All rights reserved.
+//
+
+#ifndef XXZFontAndColor_h
+#define XXZFontAndColor_h
+
+/* ***************************************************** */
+//字体大小
+#define FONT_BOLD_l4 [UIFont boldSystemFontOfSize:14*ratio]
+#define FONT_l6 [UIFont systemFontOfSize:16*ratio]
+#define FONT_l5 [UIFont systemFontOfSize:15*ratio]
+#define FONT_l4 [UIFont systemFontOfSize:14*ratio]
+#define FONT_l3 [UIFont systemFontOfSize:13*ratio]
+#define FONT_l2 [UIFont systemFontOfSize:12*ratio]
+#define FONT_l1 [UIFont systemFontOfSize:11*ratio]
+#define FONT_l0 [UIFont systemFontOfSize:10*ratio]
+
+//黑字lab高度
+#define HEIGHT_l 22*ratio
+//上边距
+#define TOP_l 12*ratio
+
+//背景色
+#define TITLECOLOR COLOR(30, 134, 221, 1)
+#define menColor COLOR(210, 52, 106, 1)
+#define BackGroundColor COLOR(236, 241, 242, 1)
+
+#endif /* XXZFontAndColor_h */
--- 加载相册图片/ViewController.m
+++ 加载相册图片/ViewController.m
@@ -17,6 +17,100 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self updateImageWithImageCount:0 imageArray:nil imageURLArray:nil isNet:0 superView:self.view];
+}
+
+- (void)updateImageWithImageCount:(NSInteger)num imageArray:(NSMutableArray *)imageArr imageURLArray:(NSMutableArray *)imageURLArr isNet:(NSInteger)integer  superView:(UIView *)view{
+    Return(num, 0)
+    //    num = imageArr.count;
+//    num = imageURLArr.count;
+    
+    num = 6;
+    
+    CGFloat width = 70*RATIO_WIDTH;
+    CGFloat distance_x = 10*RATIO_WIDTH;
+    
+    if (num == 0) {
+        UIButton *imgBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        imgBtn.frame = CGRectMake(distance_x, distance_x, width, width);
+        imgBtn.backgroundColor = [UIColor redColor];
+        imgBtn.tag = 1000+num;
+        [view addSubview:imgBtn];
+        
+        [imgBtn addTarget:self action:@selector(imgBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    } else if (num < 4) {
+        CGFloat internal = (SCREEN_WIDTH-width*4)/5;
+        
+        for (int i = 0; i < (num+1); i++) {
+            UIButton *imgBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+            imgBtn.frame = CGRectMake(internal+(width+internal)*i, distance_x, width, width);
+            imgBtn.backgroundColor = [UIColor cyanColor];
+            imgBtn.tag = 1000+i;
+            [view addSubview:imgBtn];
+            
+            if (i == num) {
+                imgBtn.backgroundColor = [UIColor redColor];
+            }
+            
+            [imgBtn addTarget:self action:@selector(imgBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+    } else if (num < 9) {
+        CGFloat internal = (SCREEN_WIDTH-width*4)/5;
+        
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                
+                NSInteger temp = i*(num+1)+j;
+              
+                
+                if (i == 1 && (j>num-4)) {
+                    return;
+                }
+                  XXZLog(@"temp = %ld", temp);
+                
+                UIButton *imgBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+                imgBtn.frame = CGRectMake(internal+(width+internal)*j, distance_x+(width+distance_x)*i, width, width);
+                imgBtn.backgroundColor = [UIColor cyanColor];
+                imgBtn.tag = 1000+temp;
+                [view addSubview:imgBtn];
+                
+                if (i == 1 && (j == num-4)) {
+                    imgBtn.backgroundColor = [UIColor redColor];
+                }
+                
+                [imgBtn addTarget:self action:@selector(imgBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+
+                
+            }
+        }
+    }
+    
+}
+
+- (void)imgBtnAction:(UIButton *)btn {
+    XXZLog(@"btn.tag = %ld", btn.tag)
+    
+}
+
+#pragma mark -
+- (void)buildLayout {
+    
+}
+
+#pragma mark -
+#pragma mark - action
+
+
+#pragma mark - getter
+
+
+#pragma mark - other
+
+
+#pragma mark - dealloc
+- (void)dealloc {
+    
 }
 
 - (void)didReceiveMemoryWarning {
